d7034845152c39b58a7ddbebeedf85e55aacf6f6
Kornkritpawit <konsoo@hotmail.com>
2021-01-13 23:07:41 +0700

    create portfolio models

---
+++ myportfolio/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+admin.site.register(Person)
+admin.site.register(Email)
+admin.site.register(SocialContact)
+admin.site.register(WorkDuration)
+admin.site.register(Work)
+admin.site.register(FrontendSkill)
+admin.site.register(BackendSkill)
+admin.site.register(ProgrammingLanguage)
+
---
+++ myportfolio/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+
+# Create your models here.
+
+class Person(models.Model):
+    firstName_text = models.CharField(max_length=20)
+    lastName_text = models.CharField(max_length=20)
+    fullName_text = models.CharField(max_length=50)
+    NickName_text = models.CharField(max_length=10)
+    phone_field = models.IntegerField()
+    image = models.ImageField()
+    aboutMe = models.TextField()
+    line = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.fullName_text
+
+class Email(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    email_field = models.EmailField()
+
+    def __str__(self):
+        return self.email_field
+
+class SocialContact(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    platform = models.CharField(max_length=20)
+    link = models.URLField()
+
+class WorkDuration(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    duration_field = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email_field
+
+class Work(models.Model):
+    workDuration = models.ForeignKey(WorkDuration, on_delete=models.CASCADE)
+    projectLink = models.URLField()
+    description = models.TextField()
+
+
+
+
+class FrontendSkill(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    skill_text = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.skill_text
+
+class BackendSkill(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    skill_text = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.skill_text
+
+class ProgrammingLanguage(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    skill_text = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.skill_text
+
+
